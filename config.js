95bb97a15dec7b723a1520d94fa01d7c736b4b4c
Thomas Di Grégorio <dig@p2p.legal>
2022-11-27 19:51:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,40 @@
 export default {
-	root: '/',
+	root: '/retest/',
 	container: 'body',
 // 	locate: href=> href.slice(-1) == '/' ? href+'index.md' : href+'.md'
 // 	locate: href=> href.replace( /\.html$/, '.md' )
 	locate: href=> href.replace( /\/$/, '.md' )
 }
+/*
+no ext and no /:
+canonical		real			data
+/			/index.html		/index.md
+/page1			/page1.html		/page1.md
+/page2			/page2.html		/page2.md
+/dir/			/dir/index.html		/dir/index.md
+/dir/subpage		/dir/subpage.html	/dir/subpage.md
+
+
+no ext and /:
+canonical		real			data
+/			/index.md
+/index.html		/index.md
+/page1/			/page1.md
+/page1/index.html	/page1.md
+/page2/			/page2.md
+/page2/index.html	/page2.md
+/dir/			/dir.md
+/dir/index.html		/dir.md
+/dir/subpage/		/dir/subpage.md
+
+
+ext and / dir:
+/			/index.md
+/index.html		/index.md
+/page1.html		/page1.md
+/page2.html		/page2.md
+/dir/index.html		/dir.md		/dir/index.md
+/dir/			/dir.md		/dir/index.md
+/dir/subpage.html	/dir/subpage.md
+
+*/
