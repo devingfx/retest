3f5c092d15e81bf1b7a9e8f0178bef8eaa76bce9
Thomas Di Grégorio <dig@p2p.legal>
2022-11-30 20:13:07 +0100

    Update and rename config.js to .spa.config.js

--- .spa.config.js
+++ .spa.config.js
@@ -1,5 +1,5 @@
 export default {
-	root: '/retest/',
+	root: 'https://devingfx.github.io/retest/',
 	container: 'body',
 	locate: href=> href.slice(-1) == '/' ? href+'index.md' : href+'.md'
 // 	locate: href=> href.replace( /\.html$/, '.md' )
